0a30571b6da3697ba25b2f2fc638926610d18310
Manuel Alén Sánchez <97231769+manuelalen@users.noreply.github.com>
2025-12-16 01:25:44 +0100

    Add SQL tables for products and metadata
    Created tables for products, metadata, and production times in the dev schema.

---
+++ mysql/tablas/tablas.sql
@@ -0,0 +1,24 @@
+CREATE TABLE dev.m_productos(
+ID VARCHAR(99),
+nombre VARCHAR(99)
+);
+
+-- t_metadata
+CREATE TABLE IF NOT EXISTS dev.t_metadata (
+  id INT NOT NULL AUTO_INCREMENT,
+  source JSON NOT NULL,
+  type VARCHAR(32) NOT NULL,
+  target JSON NOT NULL,
+  active BOOLEAN NOT NULL DEFAULT TRUE,
+  PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+ALTER TABLE dev.t_metadata ADD COLUMN INGESTION_NAME VARCHAR(99);
+
+-- tiempos_produccion
+CREATE TABLE dev.m_tiempos_produccion (
+  cod_producto VARCHAR(16) NOT NULL,
+  id INT NOT NULL,
+  tiempo_produccion DOUBLE NOT NULL,
+  PRIMARY KEY (cod_producto, id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
